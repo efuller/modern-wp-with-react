234a53145cb8dd0c1976b0cbc6b6f328627b45b5
Eric Fuller <info@glassmoonmedia.com>
2016-05-02 09:46:04 -0400

    localize the wp-api url script

--- inc/scripts.php
+++ inc/scripts.php
@@ -11,5 +11,9 @@ function modernwp_scripts() {
 
 	// Enqueue scripts.
 	wp_enqueue_script( 'modernwp-scripts', get_template_directory_uri() . '/assets/scripts.js', array(), $version, true );
+	wp_localize_script( 'modernwp-scripts', 'WP_API', array(
+		'api_url' => esc_url_raw( rest_url() ),
+		'nonce'   => wp_create_nonce( 'wp_rest' ),
+	) );
 }
 add_action( 'wp_enqueue_scripts', 'modernwp_scripts' );
