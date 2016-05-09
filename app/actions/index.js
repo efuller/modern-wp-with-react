cde3b8a041ace99f75335958aabb33150a8b182f
Eric Fuller <info@glassmoonmedia.com>
2016-05-09 10:22:37 -0400

    fetch posts action

---
+++ app/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { FETCH_POSTS } from './types';
+
+const ROOT_URL = "http://wpapi.dev/wp-json/wp/v2";
+
+export function fetchPosts() {
+    return function(dispatch) {
+        axios.get(`${ROOT_URL}/posts`, {
+            headers: { 'X-WP-Nonce': WP_API.nonce }
+        })
+            .then(response => {
+                //console.log(response.data);
+                dispatch({
+                    type: FETCH_POSTS,
+                    payload: response.data
+                });
+            });
+    }
+}
