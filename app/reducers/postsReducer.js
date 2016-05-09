f55c5566f555b9cd25509b7b1b76f78257ed34cd
Eric Fuller <info@glassmoonmedia.com>
2016-05-09 10:23:15 -0400

    get a posts reducer setup

--- app/reducers/postsReducer.js
+++ app/reducers/postsReducer.js
@@ -1,6 +1,12 @@
-const INITIAL_STATE = {};
+const INITIAL_STATE = [];
+import { FETCH_POSTS } from '../actions/types';
 
 export default function(state = INITIAL_STATE, action) {
+    switch(action.type) {
+        case FETCH_POSTS:
+            //console.log(action.payload.length);
+            return{ ...state, posts: action.payload };
+    }
 
     return state;
 }
