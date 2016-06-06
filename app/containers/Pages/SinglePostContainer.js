0616971f881a74817de57fd9d2d0cbc7b02a961f
Eric Fuller <info@glassmoonmedia.com>
2016-06-06 10:36:36 -0400

    Add a single post container

---
+++ app/containers/Pages/SinglePostContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { SinglePost } from '../../components';
+
+class SinglePostContainer extends Component {
+    
+    render() {
+       return <SinglePost />
+    }
+}
+
+export default SinglePostContainer;
