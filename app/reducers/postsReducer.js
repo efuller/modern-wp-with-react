39d3e4c44339895fc5c5189a240ab0f232c24c6d
Eric Fuller <info@glassmoonmedia.com>
2016-06-20 09:25:58 -0400

    Create fetchPost reducer.

--- app/reducers/postsReducer.js
+++ app/reducers/postsReducer.js
@@ -1,10 +1,14 @@
 const INITIAL_STATE = [];
-import { FETCH_POSTS } from '../actions/types';
+import {
+    FETCH_POSTS,
+    FETCH_POST } from '../actions/types';
 
 export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
         case FETCH_POSTS:
-            return{ ...state, posts: action.payload };
+            return { ...state, posts: action.payload };
+        case FETCH_POST:
+            return { ...state, post: action.payload };
     }
 
     return state;
