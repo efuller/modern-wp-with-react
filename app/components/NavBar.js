7edd5a4c46346fd1bfd3758af14f2580b299a32c
Eric Fuller <info@glassmoonmedia.com>
2016-07-29 16:11:45 -0400

    Add a title to the navbar with some styles.

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import AppBar from 'material-ui/AppBar';
 
 const styles = {
     root: {
         backgroundColor: '#303F9F'
+    },
+    brand: {
+        color: '#FFFFFF'
     }
 };
 
 class NavBar extends Component {
 
     render() {
-        return <AppBar style={styles.root}/>
+        return <AppBar title={<Link style={styles.brand} to="/">Modern WP with React</Link>} style={styles.root}/>
     }
 }
 
