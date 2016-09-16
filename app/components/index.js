365b06e5ea7ba31f8d059fedb331e07f98eb6ec1
Eric Fuller <info@glassmoonmedia.com>
2016-09-15 22:25:13 -0400

    Refactor App.js out into a Container component.

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,2 @@
-export App from './App';
 export PostList from './PostList';
 export SinglePost from './SinglePost';
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import { history } from '../store/configureStore';
 
+// Containers
+import { AppContainer } from '../containers';
+
 import {
-    App,
     PostList,
     SinglePost } from '../components';
 
 export default (
     <Router history={history}>
-        <Route path="/" component={App}>
+        <Route path="/" component={AppContainer}>
             <IndexRoute component={PostList} />
             <Route path="/:slug" component={SinglePost} />
         </Route>
