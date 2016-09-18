5ab8bdd97a0a068003c58a7742d0b40e475065f9
Eric Fuller <info@glassmoonmedia.com>
2016-09-18 18:57:38 -0400

    Use a home container component.

---
+++ app/containers/Home/HomeContainer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions/postActions';
+import { Link } from 'react-router';
+import LoadingCircular from '../../components/elements/CircularProgress';
+import Paper from 'material-ui/Paper';
+
+const styles = {
+	root: {
+		marginBottom: '20px',
+		padding: '20px'
+	}
+};
+
+class HomeContainer extends Component {
+
+	renderPost() {
+		if(! this.props.isFetched) {
+			return <LoadingCircular/>
+		}
+
+		return this.props.posts.map((post) => {
+			return (
+				<Paper key={post.id} style={styles.root}>
+					<article className="card">
+						<Link to={post.slug}>
+							<h2 className="post-title">{post.title.rendered}</h2>
+						</Link>
+						<div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}} />
+					</article>
+				</Paper>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				{this.renderPost()}
+			</div>
+		)
+	}
+}
+
+function mapStateToProps(state) {
+	return { posts: state.posts.posts, isFetched: state.posts.isFetched };
+}
+
+export default connect(mapStateToProps, actions)(HomeContainer);
