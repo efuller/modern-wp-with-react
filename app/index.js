e9c711b5e9050204c1a4e075211561e1403260c8
Eric Fuller <info@glassmoonmedia.com>
2016-09-18 18:57:09 -0400

    Hydrate state with posts on app load rather than inside the HomeContainer.

--- app/index.js
+++ app/index.js
@@ -5,12 +5,14 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
 import { fetchCategories } from './actions/categoryActions';
+import { fetchPosts } from './actions/postActions';
 import routes from './routes/routes';
 
 // Styles
 var styles = require('./scss/style.scss');
 
 store.dispatch(fetchCategories());
+store.dispatch(fetchPosts());
 
 const Root = () => (
     <Provider store={store}>
