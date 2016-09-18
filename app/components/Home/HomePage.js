2adb1f4b4dfa0b9caa138b90730c3aef60f5e578
Eric Fuller <info@glassmoonmedia.com>
2016-09-18 19:42:17 -0400

    Create a stateless HomePage component. Set it up to use the PostList component.

---
+++ app/components/Home/HomePage.js
@@ -0,0 +1,35 @@
+import React, {PropTypes} from 'react';
+import Paper from 'material-ui/Paper';
+import { Link } from 'react-router';
+
+const styles = {
+	root: {
+		marginBottom: '20px',
+		padding: '20px'
+	}
+};
+
+// @todo Refactor this out into a stateless component
+function PostCard({post}) {
+	return (
+		<Paper style={styles.root}>
+			<article className="card">
+				<Link to={post.slug}>
+					<h2 className="post-title">{post.title.rendered}</h2>
+				</Link>
+				<div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}/>
+			</article>
+		</Paper>
+	)
+}
+
+
+const HomePage = ({posts}) => {
+	return (
+		<div>
+			{posts.map((post) => ( <PostCard key={post.id} post={post} /> ))}
+		</div>
+	)
+};
+
+export default HomePage;
