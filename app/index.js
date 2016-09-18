226b18fe7a876aef5e5045f681b85987fef98836
Eric Fuller <info@glassmoonmedia.com>
2016-09-18 18:15:27 -0400

    Hydrate state on load with categories.

--- app/index.js
+++ app/index.js
@@ -4,10 +4,13 @@ import { Router, Route, IndexRoute } from 'react-router';
 
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
+import { fetchCategories } from './actions/categoryActions';
 import routes from './routes/routes';
 
 // Styles
 var styles = require('./scss/style.scss');
+
+store.dispatch(fetchCategories());
 
 const Root = () => (
     <Provider store={store}>
