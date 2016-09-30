f69d5ee72b1089ffa6706c9bfc49d74d9300b1cd
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 16:31:13 -0400

    Get react modal setup and working.

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,3 +1,4 @@
 export AppContainer from './App/AppContainer';
 export HomeContainer from './Home/HomeContainer';
 export CategoryContainer from './Category/CategoryContainer';
+export SingleContainer from './Single/SingleContainer';
