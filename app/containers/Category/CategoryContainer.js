8a954eab6e9d5f1c24377ef0975d264a9420c1fd
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 14:51:27 -0400

    Lots of work on the CategoryContainer. Hooking it up with Redux and configure to handle link transitions.

--- app/containers/Category/CategoryContainer.js
+++ app/containers/Category/CategoryContainer.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as actions from '../../actions/postActions';
 import LoadingCircular from '../../components/elements/CircularProgress';
 import CategoryPage from '../../components/CategoryPage/CategoryPage';
 
 class CategoryContainer extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	componentWillUnmount() {
+		this.props.actions.resetCategoryPosts();
+	}
+
+	componentWillMount() {
+		this.props.actions.fetchPostsByCategoryId(this.props.params.category);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.props.currentCategory != nextProps.params.category) {
+			this.props.actions.fetchPostsByCategoryId(nextProps.params.category);
+		}
+	}
 
 	renderPost() {
-		// if(! this.props.isFetched) {
-		// 	return <LoadingCircular/>
-		// } else {
-		// 	return (
-		// 		<CategoryPage />
-		// 	)
-		// }
-		return (
-			<CategoryPage/>
-		)
+		if(! this.props.isFetched) {
+			return <LoadingCircular/>
+		} else {
+			return (
+				<CategoryPage categoryPosts={this.props.postsInCategory} />
+			)
+		}
 	}
 
 	render() {
@@ -28,9 +43,20 @@ class CategoryContainer extends Component {
 	}
 }
 
-// function mapStateToProps(state) {
-// 	return { posts: state.posts.posts, isFetched: state.posts.isFetched };
-// }
-//
-// export default connect(mapStateToProps, actions)(CategoryContainer);
-export default CategoryContainer;
+function mapStateToProps(state, ownProps) {
+	console.log('State:',state);
+	const currentCategory = ownProps.params.category;
+	return {
+		postsInCategory: state.posts.categoryPosts.data,
+		isFetched: state.posts.categoryPostsFetched,
+		currentCategory: currentCategory
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		actions: bindActionCreators(actions, dispatch)
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryContainer);
