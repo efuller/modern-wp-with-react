9f2115771849f0be6c1018e792a3ca6de3c015f2
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 15:07:53 -0400

    Display posts in a Paper card on category pages.

--- app/components/CategoryPage/CategoryPage.js
+++ app/components/CategoryPage/CategoryPage.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import { Link } from 'react-router';
+
+const styles = {
+	root: {
+		marginBottom: '20px',
+		padding: '20px'
+	}
+};
+
+function PostCard({categoryPost}) {
+	return (
+		<Paper style={styles.root}>
+			<article className="card">
+				<Link to={`/story/${categoryPost.slug}`}>
+					<h2 className="post-title">{categoryPost.title.rendered}</h2>
+				</Link>
+				<div dangerouslySetInnerHTML={{__html: categoryPost.excerpt.rendered}}/>
+			</article>
+		</Paper>
+	)
+}
 
 const CategoryPage = ({ categoryPosts }) => {
 	return (
-		<div>Category Posts</div>
+		<div>{categoryPosts.map((categoryPost) => ( <PostCard key={categoryPost.id} categoryPost={categoryPost} />))}</div>
 	)
 };
 
