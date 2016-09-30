20961447b019d81b5cbf1bc060b54c0e0242a2a2
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 10:07:08 -0400

    More cleaning up of unused imports.

--- app/containers/Home/HomeContainer.js
+++ app/containers/Home/HomeContainer.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/postActions';
-//import { Link } from 'react-router';
 import LoadingCircular from '../../components/elements/CircularProgress';
-//import Paper from 'material-ui/Paper';
 import HomePage from '../../components/Home/HomePage';
 
 class HomeContainer extends Component {
