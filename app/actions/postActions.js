83fe3a7d707f5eec63ad0a6d7946e69339cc17c2
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 14:20:39 -0400

    Add in a fetch posts by category ID action creator.

--- app/actions/postActions.js
+++ app/actions/postActions.js
@@ -2,10 +2,18 @@ import axios from 'axios';
 import {
     FETCH_POSTS,
     FETCH_POST,
-    RESET_POST } from './actionTypes';
+    RESET_POST,
+	FETCH_POSTS_BY_CATEGORY_ID } from './actionTypes';
 
 const ROOT_URL = "http://wpapi.dev/wp-json/wp/v2";
 //const ROOT_URL = "http://api.ericfuller.net/wp-json/wp/v2";
+
+export function fetchPostsByCategoryIdSuccess(posts) {
+	return {
+		type: FETCH_POSTS_BY_CATEGORY_ID,
+		payload: posts
+	}
+}
 
 export function fetchPosts() {
 
@@ -35,6 +43,20 @@ export function fetchPost(slug) {
                 });
             });
     }
+}
+
+export function fetchPostsByCategoryId(categorySlug) {
+	return function(dispatch) {
+		axios.get(`${ROOT_URL}/posts?filter[category_name]=${categorySlug}`, {
+			headers: {'X-WP-Nonce': WP_API.nonce}
+		})
+			.then(posts => {
+				dispatch(fetchPostsByCategoryIdSuccess(posts));
+			})
+			.catch(error => {
+				throw(error);
+			})
+	}
 }
 
 export function resetActivePost() {
