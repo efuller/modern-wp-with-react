10f1aae67256704349869f82ef93ebc33b01ad9d
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 16:29:24 -0400

    Use SingleContainer for single posts.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,16 +3,14 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { history } from '../store/configureStore';
 
 // Containers
-import { AppContainer, HomeContainer, CategoryContainer } from '../containers';
-
-import { SinglePost } from '../components';
+import { AppContainer, HomeContainer, CategoryContainer, SingleContainer } from '../containers';
 
 export default (
     <Router history={history}>
         <Route path="/" component={AppContainer}>
             <IndexRoute component={HomeContainer} />
 	        <Route path="category/:category" component={CategoryContainer} />
-            <Route path="story/:slug" component={SinglePost} />
+            <Route path="story/:slug" component={SingleContainer} />
         </Route>
     </Router>
 );
