7b382708b6930c2333fe4733de3c7fefb11ccadd
Eric Fuller <info@glassmoonmedia.com>
2016-09-30 16:30:28 -0400

    Convert single post into a stateless container.

--- app/components/SinglePost/SinglePost.js
+++ app/components/SinglePost/SinglePost.js
@@ -1,53 +1,23 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../../actions/postActions';
-import { bindActionCreators } from 'redux';
-import LoadingCircular from '../elements/CircularProgress';
-import Paper  from 'material-ui/Paper';
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import { Link } from 'react-router';
 
-const styles = {
-    root: {
-        marginBottom: '20px',
-        padding: '20px'
-    }
+function PostCard({post}) {
+	console.log(post);
+	return (
+		<Paper>
+			<article className="card">
+				<h2 className="post-title">{post.title.rendered}</h2>
+				<div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}/>
+			</article>
+		</Paper>
+	)
+}
+
+const SinglePost = ({ post }) => {
+	return (
+		<div><PostCard key={post.id} post={post} /></div>
+	)
 };
 
-// @todo Refactor single posts into stateless components - Data is already in state.
-class SinglePost extends Component {
-    static contextTypes = {
-        router: PropTypes.object
-    };
-
-    componentWillUnmount() {
-        this.props.actions.resetActivePost();
-    }
-
-    componentWillMount() {
-        this.props.actions.fetchPost(this.props.params.slug);
-    }
-    
-    render() {
-        if(! this.props.post) {
-            return <LoadingCircular/>;
-        }
-
-        return (
-            <Paper style={styles.root}>
-                <div>
-                    <h2 className="post-title">{this.props.post.title.rendered}</h2>
-                    <div dangerouslySetInnerHTML={{__html: this.props.post.content.rendered}} />
-                </div>
-            </Paper>
-        )
-    }
-}
-
-function mapStateToProps(state) {
-    return { post: state.posts.post }
-}
-
-function mapDispatchToProps(dispatch) {
-    return { actions: bindActionCreators(actions, dispatch) };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SinglePost);
+export default SinglePost;
