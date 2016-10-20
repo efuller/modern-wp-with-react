21f6af465746593a1a6119e81409c5cce492be53
Eric Fuller <info@glassmoonmedia.com>
2016-10-19 23:08:25 -0400

    Refactor out post actions that are no longer being used.

--- app/actions/postActions.js
+++ app/actions/postActions.js
@@ -9,13 +9,6 @@ import {
 const ROOT_URL = "http://wpapi.dev/wp-json/wp/v2";
 //const ROOT_URL = "http://api.ericfuller.net/wp-json/wp/v2";
 
-export function fetchPostsByCategoryIdSuccess(posts) {
-	return {
-		type: FETCH_POSTS_BY_CATEGORY_ID,
-		payload: posts
-	}
-}
-
 export function fetchPosts() {
 
     return function(dispatch) {
@@ -29,47 +22,4 @@ export function fetchPosts() {
                 });
             });
     }
-}
-
-export function fetchPost(slug) {
-
-    return function(dispatch) {
-        axios.get(`${ROOT_URL}/posts/?filter[name]=${slug}`, {
-            headers: { 'X-WP-Nonce': WP_API.nonce },
-        })
-            .then(response => {
-                dispatch({
-                    type: FETCH_POST,
-                    payload: response
-                });
-            });
-    }
-}
-
-export function fetchPostsByCategoryId(categorySlug) {
-	return function(dispatch) {
-		axios.get(`${ROOT_URL}/posts?filter[category_name]=${categorySlug}`, {
-			headers: {'X-WP-Nonce': WP_API.nonce}
-		})
-			.then(posts => {
-				dispatch(fetchPostsByCategoryIdSuccess(posts));
-			})
-			.catch(error => {
-				throw(error);
-			})
-	}
-}
-
-export function resetActivePost() {
-
-    return {
-        type: RESET_POST
-    }
-}
-
-export function resetCategoryPosts() {
-
-	return {
-		type: RESET_CATEGORY_POSTS
-	}
 }
