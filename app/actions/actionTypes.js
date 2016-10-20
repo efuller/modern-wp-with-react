948f952474efe79c60e41b7a6b757981ef82015e
Eric Fuller <info@glassmoonmedia.com>
2016-10-19 23:07:49 -0400

    Refactor out action types that are no longer being used.

--- app/actions/actionTypes.js
+++ app/actions/actionTypes.js
@@ -1,7 +1,3 @@
 // Post action types
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const FETCH_POST = 'FETCH_POST';
-export const RESET_POST = 'RESET_POST';
 export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
-export const FETCH_POSTS_BY_CATEGORY_ID = 'FETCH_POSTS_BY_CATEGORY_ID';
-export const RESET_CATEGORY_POSTS = 'RESET_CATEGORY_POSTS';
