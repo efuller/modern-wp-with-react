9580265e6e05f2737afcd5230de750b501924d94
Eric Fuller <info@glassmoonmedia.com>
2016-10-29 18:17:42 -0400

    Create some helper functions for finding previous/next categories and posts.

---
+++ app/helpers/utils.js
@@ -0,0 +1,21 @@
+export function getNext(items, index) {
+	if (items[index+1]) {
+		return items[index + 1];
+	}
+}
+
+export function getPrevious(items, index) {
+	if (items[index - 1]) {
+		return items[index - 1];
+	}
+}
+
+export function getCurrent(items, name) {
+	const navCurrentCategory = items.filter(item => item.slug == name);
+	if ( navCurrentCategory ) return navCurrentCategory[0];
+	return null;
+}
+
+export function getIndex(items, current) {
+	return items.findIndex(item => item.slug == current.slug);
+}
