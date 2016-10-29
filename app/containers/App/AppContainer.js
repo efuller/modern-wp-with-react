02d9d15cf770eed57d54bd0192a776d5dc07b12e
Eric Fuller <info@glassmoonmedia.com>
2016-10-29 18:32:13 -0400

    Add in category navigation.

--- app/containers/App/AppContainer.js
+++ app/containers/App/AppContainer.js
@@ -4,6 +4,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
+import ContentNavigation from '../../components/Navigation/ContentNavigation';
+import * as Utils from '../../helpers/utils';
 
 class App extends Component {
 
@@ -12,8 +14,18 @@ class App extends Component {
 			<MuiThemeProvider>
 				<div>
 					<Header/>
-					<Navigation categories={this.props.categories} />
+					<Navigation categories={this.props.categories} navCategorySlug={this.props.navCategorySlug} />
 					<div className="container">
+						{
+							(! this.props.navCategorySlug || ! this.props.navNextCategory )
+							? ''
+							: <ContentNavigation direction={`next`} category={this.props.navNextCategory}/>
+						}
+						{
+							(! this.props.navCategorySlug || ! this.props.navPreviousCategory )
+								? ''
+								: <ContentNavigation direction={`previous`} category={this.props.navPreviousCategory}/>
+						}
 						<main role="main">
 							{this.props.children}
 						</main>
@@ -24,9 +36,25 @@ class App extends Component {
 	}
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state, ownProps) {
+	let navCategorySlug = ownProps.params.category;
+	let currentIndex = '';
+	let navCurrentCategory = '';
+	let navNextCategory = '';
+	let navPreviousCategory = '';
+
+	if (navCategorySlug) {
+		navCurrentCategory = Utils.getCurrent(state.categories.categories, navCategorySlug);
+		currentIndex = Utils.getIndex(state.categories.categories, navCurrentCategory);
+		navNextCategory = Utils.getNext(state.categories.categories, currentIndex);
+		navPreviousCategory = Utils.getPrevious(state.categories.categories, currentIndex);
+	}
+
 	return {
-		categories: state.categories.categories
+		categories: state.categories.categories,
+		navCategorySlug,
+		navNextCategory,
+		navPreviousCategory
 	}
 }
 
