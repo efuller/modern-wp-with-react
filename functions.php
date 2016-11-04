719ebbbcdb93063f031b10c7207bf983d5f94d85
Eric Fuller <info@glassmoonmedia.com>
2016-11-04 15:54:21 -0400

    Add the thumbnail URL to the api post response :boom:

--- functions.php
+++ functions.php
@@ -7,3 +7,25 @@ require get_template_directory() . '/inc/scripts.php';
 
 // Enable featured images.
 add_theme_support( 'post-thumbnails' );
+
+add_action( 'rest_api_init', 'epf_insert_thumbnail_url' );
+function epf_insert_thumbnail_url() {
+	register_rest_field( 'post',
+		'thumbnail',
+		array(
+			'get_callback'    => 'epf_get_thumbnail_url',
+			'update_callback' => null,
+			'schema'          => null,
+		)
+	);
+}
+
+function epf_get_thumbnail_url( $post ){
+	if(has_post_thumbnail( $post['id']) ){
+		$imgArray = wp_get_attachment_image_src( get_post_thumbnail_id( $post['id'] ), 'full' );
+		$imgURL = $imgArray[0];
+		return $imgURL;
+	} else {
+		return false;
+	}
+}
