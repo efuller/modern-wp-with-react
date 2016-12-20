a4265bebab9b6bf5432bb4c6973b722719baf705
Eric Fuller <info@glassmoonmedia.com>
2016-12-20 07:11:14 -0500

    Add a post thumbnail image size.

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ add_theme_support( 'post-thumbnails' );
 
 // Add a featured image size.
 add_image_size( 'featured_image', 960, 400, array( 'center', 'center' ) );
+add_image_size( 'post_thumb', 640, 427, array( 'center', 'center' ) );
 
 add_action( 'rest_api_init', 'epf_insert_thumbnail_url' );
 function epf_insert_thumbnail_url() {
@@ -25,7 +26,7 @@ function epf_insert_thumbnail_url() {
 
 function epf_get_thumbnail_url( $post ){
 	if(has_post_thumbnail( $post['id']) ){
-		$imgArray = wp_get_attachment_image_src( get_post_thumbnail_id( $post['id'] ), 'featured_image' );
+		$imgArray = wp_get_attachment_image_src( get_post_thumbnail_id( $post['id'] ), 'post_thumb' );
 		$imgURL = $imgArray[0];
 		return $imgURL;
 	} else {
